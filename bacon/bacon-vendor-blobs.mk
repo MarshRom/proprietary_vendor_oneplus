18eae0f82784198c592aec31153a8a73a96049c1
Aaahh Ahh <woohoomoo2u@gmail.com>
2016-05-05 00:38:46 -0400

    Update bacon-vendor-blobs.mk

--- bacon/bacon-vendor-blobs.mk
+++ bacon/bacon-vendor-blobs.mk
@@ -195,7 +195,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/bacon/proprietary/vendor/firmware/libpn547_fw.so:system/vendor/firmware/libpn547_fw.so \
     vendor/oneplus/bacon/proprietary/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
     vendor/oneplus/bacon/proprietary/vendor/lib/libAKM8963.so:system/vendor/lib/libAKM8963.so \
-    vendor/oneplus/bacon/proprietary/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     vendor/oneplus/bacon/proprietary/vendor/lib/libMA3-wavesfx-Coretex_A9.so:system/vendor/lib/libMA3-wavesfx-Coretex_A9.so \
     vendor/oneplus/bacon/proprietary/vendor/lib/libMA3-wavesfx-Qualcomm.so:system/vendor/lib/libMA3-wavesfx-Qualcomm.so \
     vendor/oneplus/bacon/proprietary/vendor/lib/libgnustl_shared.so:system/vendor/lib/libgnustl_shared.so \
